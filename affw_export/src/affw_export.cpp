28ec0086dee9684b1cc56057c90e51a8d31e4277
Nicolai Ommer <nicolai.ommer@gmail.com>
2016-05-15 18:32:57 +0200

    changed exporting of data
     * everything in one package
     * collect first, write into file in batch mode
     * collect all data in same process and save on exit

--- affw_export/src/affw_export.cpp
+++ affw_export/src/affw_export.cpp
@@ -8,6 +8,7 @@
 #include <affw_msgs/ProcTime.h>
 #include <affw_msgs/TargetRequest.h>
 #include <boost/filesystem/operations.hpp>
+#include <boost/thread.hpp>
 #include <ros/init.h>
 #include <ros/node_handle.h>
 #include <ros/param.h>
@@ -15,84 +16,183 @@
 #include <ros/time.h>
 #include <std_msgs/Header.h>
 #include <std_msgs/String.h>
+#include <nav_msgs/Odometry.h>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <signal.h>
+#include <tf/tf.h>
 
+#define LOG_AFFW 0
+#define LOG_PROC_TIME 1
+#define LOG_STATE 2
+#define LOG_ODOM 3
+#define LOG_SET 4
+#define NUM_LOGFILES 5
+
+std::string logFiles[] = {"affw.csv","procTime.csv","globalState.csv", "localVel.csv", "set.csv"};
+std::vector< std::vector<float> > data[NUM_LOGFILES];
+std::vector< std::vector<float> > dataAll[NUM_LOGFILES];
 std::string folder = "/tmp/affw_data";
-std::ofstream fAffw, fProcTime;
-bool first = true;
+std::string folderCustom;
+
+double getOrientation(geometry_msgs::Quaternion gq) {
+	double ow = gq.w;
+	double ox = gq.x;
+	double oy = gq.y;
+	double oz = gq.z;
+
+	tf::Quaternion q(ox, oy, oz, ow);
+	tf::Matrix3x3 m(q);
+	double roll, pitch, yaw;
+	m.getRPY(roll, pitch, yaw);
+	return yaw;
+}
 
 void callbackAffw(const affw_msgs::TargetRequest::ConstPtr& tr) {
 
-	if(first)
+	if(data[LOG_AFFW].empty() && !folderCustom.empty())
+	{
+		std::ofstream fDim(std::string(folderCustom + "/dimensions.csv").c_str());
+		fDim << tr->state.size() << " " << tr->target.size() << std::endl;
+		fDim.close();
+	}
+	if(dataAll[LOG_AFFW].empty())
 	{
 		std::ofstream fDim(std::string(folder + "/dimensions.csv").c_str());
 		fDim << tr->state.size() << " " << tr->target.size() << std::endl;
 		fDim.close();
-		first = false;
 	}
 
-	fAffw << std::fixed << std::setw(11) << std::setprecision(6)
-			<< tr->header.stamp.toSec();
+	std::vector<float> ds;
+
+	ds.push_back(tr->header.stamp.toSec());
 
 	for (int i = 0; i < tr->state.size(); i++)
-		fAffw << " " << tr->state[i];
+		ds.push_back(tr->state[i]);
 	for (int i = 0; i < tr->target.size(); i++)
-		fAffw << " " << tr->target[i];
+		ds.push_back(tr->target[i]);
 	for (int i = 0; i < tr->action.size(); i++)
-		fAffw << " " << tr->action[i];
+		ds.push_back(tr->action[i]);
 	for (int i = 0; i < tr->actionComp.size(); i++)
-		fAffw << " " << tr->actionComp[i];
+		ds.push_back(tr->actionComp[i]);
 
-	fAffw << " " << tr->timeAction2StateOffset << " " << tr->timeStateDelayOffset;
+	ds.push_back(tr->timeAction2StateOffset.toSec());
+	ds.push_back(tr->timeStateDelayOffset.toSec());
 
-	fAffw << std::endl;
+	data[LOG_AFFW].push_back(ds);
+	dataAll[LOG_AFFW].push_back(ds);
 }
 
 void callbackProcTime(const affw_msgs::ProcTime::ConstPtr& proc) {
 
-	fProcTime << std::fixed << std::setw(11) << std::setprecision(6)
-			<< proc->stamp.toSec() << " " << (int) (proc->type) << " "
-			<< proc->procTime << std::endl;
+	std::vector<float> ds;
+	ds.push_back(proc->stamp.toSec());
+	ds.push_back(proc->type);
+	ds.push_back(proc->procTime);
+	data[LOG_PROC_TIME].push_back(ds);
+	dataAll[LOG_PROC_TIME].push_back(ds);
 }
 
-void openFiles()
-{
-	// close log files
-	if(fAffw.is_open()) fAffw.close();
-	if(fProcTime.is_open()) fProcTime.close();
+void callbackState(const nav_msgs::Odometry::ConstPtr& odom) {
 
-	if(folder.empty())
+	std::vector<float> ds;
+	ds.push_back(odom->header.stamp.toSec());
+	ds.push_back(odom->pose.pose.position.x);
+	ds.push_back(odom->pose.pose.position.y);
+	ds.push_back(getOrientation(odom->pose.pose.orientation));
+	ds.push_back(odom->twist.twist.linear.x);
+	ds.push_back(odom->twist.twist.linear.y);
+	ds.push_back(odom->twist.twist.angular.z);
+	data[LOG_STATE].push_back(ds);
+	dataAll[LOG_STATE].push_back(ds);
+}
+
+void callbackOdom(const nav_msgs::Odometry::ConstPtr& odom) {
+
+	std::vector<float> ds;
+	ds.push_back(odom->header.stamp.toSec());
+	ds.push_back(odom->twist.twist.linear.x);
+	ds.push_back(odom->twist.twist.linear.y);
+	ds.push_back(odom->twist.twist.angular.z);
+	data[LOG_ODOM].push_back(ds);
+	dataAll[LOG_ODOM].push_back(ds);
+}
+
+void callbackSet(const geometry_msgs::Twist::ConstPtr& twist) {
+
+	std::vector<float> ds;
+	ds.push_back(ros::Time::now().toSec());
+	ds.push_back(twist->linear.x);
+	ds.push_back(twist->linear.y);
+	ds.push_back(twist->angular.z);
+	data[LOG_SET].push_back(ds);
+	dataAll[LOG_SET].push_back(ds);
+}
+
+void save(std::string& folder, std::string& logFile, std::vector<std::vector<float> >& dat)
+{
+	std::string filepath = folder + "/" + logFile;
+	boost::filesystem::remove(filepath);
+	std::ofstream f;
+	f.open(filepath.c_str());
+	f << std::fixed << std::setw(11) << std::setprecision(6);
+	for(std::vector<std::vector<float> >::iterator it = dat.begin(); it != dat.end(); it++)
 	{
-		return;
+		std::vector<float> ds = *it;
+		for(std::vector<float>::iterator it2 = ds.begin(); it2 != ds.end(); it2++)
+		{
+			f << *it2 << " ";
+		}
+		f << std::endl;
+	}
+	f.close();
+}
+
+void saveAll()
+{
+	if(!folderCustom.empty())
+	{
+		for(int i=0;i<NUM_LOGFILES;i++)
+		{
+			save(folderCustom, logFiles[i], data[i]);
+			data[i].clear();
+		}
+	}
+}
+
+void mySigintHandler(int sig)
+{
+	saveAll();
+
+	for(int i=0;i<NUM_LOGFILES;i++)
+	{
+		save(folder, logFiles[i], dataAll[i]);
 	}
 
-	std::string affwFile = folder + "/affw.csv";
-	std::string procTimeFile = folder + "/procTime.csv";
-
-	// create folder if it does not exist
-	boost::filesystem::create_directories(folder);
-	// remove old files if they exist
-	boost::filesystem::remove(affwFile);
-	boost::filesystem::remove(procTimeFile);
+	// All the default sigint handler does is call shutdown()
+	ros::shutdown();
+}
 
 
-	// open log files
-	fAffw.open(affwFile.c_str());
-	fProcTime.open(procTimeFile.c_str());
+void changeDataFolder(std::string dataFolder)
+{
+	saveAll();
 
-	first = true;
+	folderCustom = dataFolder;
+	if(!folderCustom.empty())
+		boost::filesystem::create_directories(folderCustom);
+	for(int i=0;i<NUM_LOGFILES;i++)
+		data[i].clear();
 }
 
 void callbackDataFolder(const std_msgs::String::ConstPtr& dataFolder)
 {
-	if(folder != dataFolder->data)
+	if(folderCustom != dataFolder->data)
 	{
-		folder = dataFolder->data;
-		openFiles();
+		changeDataFolder(dataFolder->data);
 	}
 }
 
@@ -103,25 +203,21 @@ int main(int argc, char **argv) {
 	bool split = false;
 	ros::param::get("split", split);
 
-	ros::Subscriber sub_dataFolder;
-	if(split) {
-		sub_dataFolder = n.subscribe("dataFolder", 1,	callbackDataFolder);
-	} else {
-		openFiles();
-	}
+	ros::Subscriber sub_dataFolder = n.subscribe("dataFolder", 1, callbackDataFolder);
 
 	// subscribe to topics
 	ros::Subscriber sub_affw = n.subscribe("/affw_ctrl/target_request", 50,
 			callbackAffw);
 	ros::Subscriber sub_procTime = n.subscribe("/affw_ctrl/proc_time", 50,
 			callbackProcTime);
+	ros::Subscriber sub_odom = n.subscribe("odom", 50, callbackOdom);
+	ros::Subscriber sub_state = n.subscribe("state", 50, callbackState);
+	ros::Subscriber sub_set = n.subscribe("cmd_vel", 50, callbackSet);
+
+	signal(SIGINT, mySigintHandler);
 
 	// go
 	ros::spin();
-
-	// close log files
-	fAffw.close();
-	fProcTime.close();
 
 	return 0;
 }
