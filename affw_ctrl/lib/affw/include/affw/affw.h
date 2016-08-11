8b3fa8f539ecc1034c750e23ef488ca848e99b49
Nicolai Ommer <nicolai.ommer@gmail.com>
2016-08-11 10:39:15 +0200

    made all libraries optional

--- affw_ctrl/lib/affw/include/affw/affw.h
+++ affw_ctrl/lib/affw/include/affw/affw.h
@@ -13,17 +13,25 @@
 #include "Config.h"
 
 #include "learner/DummyLearner.h"
-#include "learner/LWPRLearner.h"
-#include "learner/FANNLearner.h"
 #include "learner/ModelLearner.h"
 #include "learner/FeedbackController.h"
+#ifdef AFFW_LWPR
+#include "learner/LWPRLearner.h"
 #include "learner/WrapperLearner.h"
+#endif
+#ifdef AFFW_FANN
+#include "learner/FANNLearner.h"
+#endif
+#ifdef AFFW_OTL
 #include "learner/OESGPLearner.h"
 #include "learner/STORKGPLearner.h"
 #include "learner/SOGPLearner.h"
 #include "learner/RLSLearner.h"
 #include "learner/RLSESNLearner.h"
+#endif
+#ifdef AFFW_OSVR
 #include "learner/OSVRLearner.h"
+#endif
 
 #include "mapping/KTermStateTarget2ActionCompMapper.h"
 
--- affw_ctrl/lib/affw/src/util/affw_factory.cpp
+++ affw_ctrl/lib/affw/src/util/affw_factory.cpp
@@ -7,41 +7,47 @@
 
 #include "affw/affw.h"
 
-affw::ModelLearner* createAffwLearner(std::string& learner_type, affw::Config& config)
-{
+affw::ModelLearner* createAffwLearner(std::string& learner_type,
+		affw::Config& config) {
 	affw::ModelLearner* learner = 0;
-	if(learner_type == affw::FANNLearner::name())
-	{
-		learner = new affw::FANNLearner(config);
-	} else if(learner_type == affw::FeedbackController::name())
-	{
+	if (learner_type == affw::FeedbackController::name()) {
 		learner = new affw::FeedbackController(config);
-	}  else if(learner_type == affw::LWPR_Learner::name())
+	}
+#ifdef AFFW_LWPR
+	else if(learner_type == affw::LWPR_Learner::name())
 	{
 		learner = new affw::LWPR_Learner(config);
-	} else if(learner_type == affw::OESGPLearner::name())
-	{
-		learner = new affw::OESGPLearner(config);
-	} else if(learner_type == affw::RLSESNLearner::name())
-	{
-		learner = new affw::RLSESNLearner(config);
-	} else if(learner_type == affw::RLSLearner::name())
-	{
-		learner = new affw::RLSLearner(config);
-	} else if(learner_type == affw::SOGPLearner::name())
-	{
-		learner = new affw::SOGPLearner(config);
-	} else if(learner_type == affw::STORKGPLearner::name())
-	{
-		learner = new affw::STORKGPLearner(config);
-	} else if(learner_type == affw::OSVRLearner::name())
-	{
-		learner = new affw::OSVRLearner(config);
 	} else if(learner_type == affw::WrapperLearner::name())
 	{
 		learner = new affw::WrapperLearner(config);
-	} else
-	{
+	}
+#endif
+
+#ifdef AFFW_OTL
+	else if (learner_type == affw::OESGPLearner::name()) {
+		learner = new affw::OESGPLearner(config);
+	} else if (learner_type == affw::RLSESNLearner::name()) {
+		learner = new affw::RLSESNLearner(config);
+	} else if (learner_type == affw::RLSLearner::name()) {
+		learner = new affw::RLSLearner(config);
+	} else if (learner_type == affw::SOGPLearner::name()) {
+		learner = new affw::SOGPLearner(config);
+	} else if (learner_type == affw::STORKGPLearner::name()) {
+		learner = new affw::STORKGPLearner(config);
+	}
+#endif
+
+#ifdef AFFW_OSVR
+	else if (learner_type == affw::OSVRLearner::name()) {
+		learner = new affw::OSVRLearner(config);
+	}
+#endif
+#ifdef AFFW_FANN
+	else if (learner_type == affw::FANNLearner::name()) {
+		learner = new affw::FANNLearner(config);
+	}
+#endif
+	else {
 		learner = new affw::DummyLearner(config);
 	}
 
